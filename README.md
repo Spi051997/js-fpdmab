1fc462eded0da09a358dffaf5fdb7e776497eddc
Spi051997 <dcodeacedemy97@gmail.com>
2022-06-28 14:11:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fpdmab
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fpdmab)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const property = 'Fistname';
+const valuee = 'Shubham';
+const user = {
+  [property]: valuee,
+};
+
+// delete user['name of book '];
+
+console.log(user);
