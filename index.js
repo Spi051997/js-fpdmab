90a0bfc02cce0698494ac2b8f345326a8905f065
Spi051997 <dcodeacedemy97@gmail.com>
2022-06-28 15:06:06 +0530

    1 file modified

--- index.js
+++ index.js
@@ -12,10 +12,47 @@ const user = {
 
 // delete user['name of book '];
 
-console.log(user);
+// console.log(user);
 
 // Print  key with loop
 
-for (let i in user) {
-  console.log(user[i]);
+// for (let i in user) {
+//   console.log(user[i]);
+// }
+
+// Quest.1
+
+const obj = {
+  a: 'one',
+  b: 'two',
+  a: 'three',
+};
+
+console.log(obj);
+// if we have two same key with different value then
+//last will be replaced with first
+// -----Output
+//{a: "three", b: "two"}
+// a: "three"
+// b: "two"
+// __proto__: Object
+
+//Quest:2:create a function multiplyby2(obj) that multipies
+// all numaric  property values of num by 2
+
+const nums = {
+  a: 100,
+  b: 200,
+  title: 'Mynums',
+};
+
+function multi(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == 'number') {
+      obj[key] = obj[key] * 2;
+    }
+  }
 }
+multi(nums);
+
+console.log(nums);
